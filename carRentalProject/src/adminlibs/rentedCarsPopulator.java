a715163fc7d680ebba5dc25e212644dbacbaea8a
Jied Joshua <118013854+jiedjoshua@users.noreply.github.com>
2023-10-13 14:01:54 +0800

    Add files via upload

---
+++ carRentalProject/src/adminlibs/rentedCarsPopulator.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adminlibs;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import carrentalproject.rentedCarsTable;
+import databasecon.ConnectionManager;
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author HP
+ */
+public class rentedCarsPopulator implements ITablePopulator {
+    
+    private Connection con;
+    private rentedCarsTable rc;
+
+    public rentedCarsPopulator(rentedCarsTable rc) {
+        this.con = ConnectionManager.getConnection();
+        this.rc = rc;
+    }
+   
+
+    @Override
+    public void tablepopulator() {
+        try {
+        int total = 0;
+        int carid = 0;
+
+        Statement s = con.createStatement();
+        ResultSet rs = s.executeQuery("SELECT username, carid, month, day, year, total FROM rentedcars");
+
+        DefaultTableModel model = new DefaultTableModel();
+        JTable jTable1 = rc.getjTable1(); // Assuming rc is initialized properly
+        jTable1.setModel(model);
+
+        // Add columns to the table model
+        model.addColumn("Username");
+        model.addColumn("Car");
+        model.addColumn("Transmission");
+        model.addColumn("Month");
+        model.addColumn("Day");
+        model.addColumn("Year");
+        model.addColumn("Total Amount");
+
+        while (rs.next()) {
+            String username = rs.getString("username");
+            carid = rs.getInt("carid");
+            String month = rs.getString("month");
+            String day = rs.getString("day");
+            String year = rs.getString("year");
+            total = rs.getInt("total");
+
+            String brand = "";
+            String transmission = "";
+            String modelcar = "";
+
+            // Retrieve car information using carid
+            String carSql = "SELECT brand, model, transmission FROM sedan WHERE carid = ?";
+            String suvSql = "SELECT brand, model, transmission FROM suv WHERE suvid = ?";
+
+            // Create separate PreparedStatements for car and suv queries
+            try (PreparedStatement carStatement = con.prepareStatement(carSql);
+                 PreparedStatement suvStatement = con.prepareStatement(suvSql)) {
+                carStatement.setInt(1, carid);
+                suvStatement.setInt(1, carid);
+
+                ResultSet carInfo = carStatement.executeQuery();
+                ResultSet suvInfo = suvStatement.executeQuery();
+
+                if (carInfo.next()) {
+                    brand = carInfo.getString("brand");
+                    modelcar = carInfo.getString("model");
+                    transmission = carInfo.getString("transmission");
+                } else if (suvInfo.next()) {
+                    brand = suvInfo.getString("brand");
+                    modelcar = suvInfo.getString("model");
+                    transmission = suvInfo.getString("transmission");
+                }
+
+                brand = brand + " " + modelcar;
+
+                // Add data to the table model
+                model.addRow(new Object[]{username, brand, transmission, month, day, year, total});
+            }
+        }
+    } catch (SQLException e) {
+        e.printStackTrace(); // Handle the exception appropriately in your application
+    }
+    }
+
+}
+
+    
+
---
+++ carRentalProject/src/rentAcarLibs/carSort.java
@@ -0,0 +1,84 @@
+package rentAcarLibs;
+
+
+// Abstract that will be used in the following classes:
+// 1. sortSedan
+// 2. sortSuv
+
+//Note: It will sort the brand and transmission for the rentnow Page
+
+
+import databasecon.ConnectionManager;
+import java.sql.Connection;
+import javax.swing.JComboBox;
+
+abstract class carSort {
+     final Connection con;
+     JComboBox<String> jc;
+     String selectedBrand;
+     
+
+    public carSort(JComboBox<String> jc, String selectedBrand) {
+        this.con = ConnectionManager.getConnection();
+        this.jc = jc;
+        this.selectedBrand = selectedBrand;
+    }
+
+    public abstract void sortBrand(); /*{
+        try {
+            // Clear existing items in the JComboBox
+            jc.removeAllItems();
+
+            // Prepare SQL statement with a placeholder for selected brand
+            String sql = "SELECT model FROM sedan WHERE brand = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, selectedBrand);
+            ResultSet rs = ps.executeQuery();
+
+            // Populate the JComboBox with models of the selected brand
+            while (rs.next()) {
+                String modelValue = rs.getString("model");
+                jc.addItem(modelValue);
+            }
+
+            // Close the ResultSet and PreparedStatement
+            rs.close();
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    } */
+    
+   public abstract void sortTransmission(); /*{
+       try {
+            // Clear existing items in the JComboBox
+            jc.removeAllItems();
+
+            // Prepare SQL statement with a placeholder for selected brand
+            String sql = "SELECT transmission FROM sedan WHERE model = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, selectedBrand);
+            ResultSet rs = ps.executeQuery();
+            System.out.println("Selected Model: " + selectedBrand);
+
+
+            // Populate the JComboBox with models of the selected brand
+            while (rs.next()) {
+                String modelValue = rs.getString("transmission");
+                jc.addItem(modelValue);
+                System.out.println(modelValue);
+            }
+
+            // Close the ResultSet and PreparedStatement
+            rs.close();
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+   } */
+   
+   
+ 
+   
+           
+}
